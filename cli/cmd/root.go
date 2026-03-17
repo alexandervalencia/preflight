03396c12109096996d80c94ed4d78475d4ab2696
Alex Valencia <alex.valencia@me.com>
2026-03-17 10:44:28 -0500

    initialize Go CLI module with cobra

---
+++ cli/cmd/root.go
@@ -0,0 +1,20 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "preflight",
+	Short: "Local PR review before pushing to GitHub",
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
+}
---
+++ cli/main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/zhubert/preflight-cli/cmd"
+
+func main() {
+	cmd.Execute()
+}
